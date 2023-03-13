a5c22330aa9b092b23a178b034da4cecab1ddf8c
AJ Arnolie <ajarno@stanford.edu>
2023-03-12 21:27:07 -0700

    Updates to Chess GUI Functionality

--- chessGUI.py
+++ chessGUI.py
@@ -19,7 +19,7 @@ m = MonteCarloTreeSearch()
 game = Silverman45ChessBoard()
 p = RandomPlayer()
 wld = [0, 0, 0]
-turn = 0
+turn = random.randint(0, 1)
 # ----------------------------------------
 
 def possible_actions_string():
@@ -29,7 +29,7 @@ def possible_actions_string():
     actions = game.legal_actions()
     info = m.get_move_info(game)
     for i in range(len(actions)):
-        s += str(i+1) + ". " + str(actions[i]) + " --- Q(s, a) = " + str(round(info[i][0], 3)) + ",  N(s, a) = " + str(info[i][1]) + "\n"
+        s += str(i+1) + ". " + str(actions[i]) + " Q(s, a) = " + str(round(info[i][0], 3)) + ",  N(s, a) = " + str(info[i][1]) + "\n"
     return s
 
 root = Tk()
@@ -41,7 +41,19 @@ game_status_var = StringVar(root, "Game Status: AbstractBoardStatus.ONGOING\n")
 white_utility_var = StringVar(root, "White Utility: 0.0")
 black_utility_var = StringVar(root, "Black Utility: -0.0\n")
 possible_actions_var = StringVar(root, possible_actions_string())
-wld_var = StringVar(root, "0 - 0 - 0 (Wins, Losses, Draws)")
+wld_var = StringVar(root, "0 - 0 - 0 (Wins, Losses, Draws)\n\n")
+white_player_name = StringVar(root, "White Player: ")
+black_player_name = StringVar(root, "Black Player: \n\n")
+update_Qvalues = StringVar(root, "Update Q")
+player1 = StringVar(root, "MCTS")
+player2 = StringVar(root, "Random")
+
+if turn == 0:
+    white_player_name.set('White Player: ' + player2.get())
+    black_player_name.set('Black Player: ' + player1.get() + "\n\n")
+else:
+    white_player_name.set('White Player: ' + player1.get())
+    black_player_name.set('Black Player: ' + player2.get() + "\n\n")
 
 # Establish Main Frames
 top_frame = Frame(root, width=1000, height=50, pady=3)
@@ -66,9 +78,6 @@ center.grid_columnconfigure(1, weight=1)
 
 board = Frame(center, width=480, height=600)
 board_tiles = [[None for _ in range(4)] for _ in range(5)]
-board_images = [[None for _ in range(4)] for _ in range(5)]
-
-img = PhotoImage(file = "./images/black_king.png")
 
 l = game.populate_board()
 for i in range(5):
@@ -80,17 +89,22 @@ for i in range(5):
         board_tiles[i][j].grid(column=j, row=i)
 
 ctr_mid = Frame(center, width=250, height=600, padx=3, pady=3)
+white_player = Label(ctr_mid, textvariable=white_player_name, font=('Modern', 16, 'bold'), justify=LEFT, anchor="w")
+black_player = Label(ctr_mid, textvariable=black_player_name, font=('Modern', 16, 'bold'), justify=LEFT, anchor="w")
 game_status = Label(ctr_mid, textvariable=game_status_var, font=('Modern', 16, 'bold'), justify=LEFT, anchor="w")
 white_utility = Label(ctr_mid, textvariable=white_utility_var, font=('Modern', 16, 'bold'), justify=LEFT, anchor="w")
 black_utility = Label(ctr_mid, textvariable=black_utility_var, font=('Modern', 16, 'bold'), justify=LEFT, anchor="w")
 possible_actions = Label(ctr_mid, textvariable=possible_actions_var, font=('Modern', 16, 'bold'), justify=LEFT, anchor="w")
 wld_label = Label(ctr_mid, textvariable=wld_var, font=('Modern', 16, 'bold'), justify=LEFT, anchor="w")
 
-game_status.grid(sticky = W, column=0, row=0)
-white_utility.grid(sticky = W, column=0, row=1)
-black_utility.grid(sticky = W, column=0, row=2)
-possible_actions.grid(sticky = W, column=0, row=3)
-wld_label.grid(sticky = W, column=0, row=4)
+white_player.grid(sticky = W, column=0, row=0)
+black_player.grid(sticky = W, column=0, row=1)
+wld_label.grid(sticky = W, column=0, row=2)
+game_status.grid(sticky = W, column=0, row=3)
+white_utility.grid(sticky = W, column=0, row=4)
+black_utility.grid(sticky = W, column=0, row=5)
+possible_actions.grid(sticky = W, column=0, row=6)
+
 
 ctr_right = Frame(center, width=100, height=600, padx=3, pady=3)
 
@@ -106,11 +120,15 @@ def update_board():
     possible_actions_var.set(possible_actions_string())
     if game.status == AbstractBoardStatus.ONGOING:
         if turn == 0:
-            _, proposed = p.propose_action(game, None, game.legal_action_mask())
-            #print("MOVE (Random):", proposed)
+            if player2.get() == "MCTS":
+                proposed = m.run_sims(game) if update_Qvalues.get() == "Update Q" else m.make_move(game)
+            else:
+                _, proposed = p.propose_action(game, None, game.legal_action_mask())
         if turn == 1:
-            proposed = m.run_sims(game)
-            #print("MOVE (MCTS):", proposed)
+            if player1.get() == "MCTS":
+                proposed = m.run_sims(game) if update_Qvalues.get() == "Update Q" else m.make_move(game)
+            else:
+                _, proposed = p.propose_action(game, None, game.legal_action_mask())
         turn = 1 - turn
         game.push(proposed)
     l = game.populate_board()
@@ -129,15 +147,23 @@ def run_game():
     turn = random.randint(0, 1)
     init_turn = turn
     if turn == 0:
-        print("Random - WHITE, MCTS - BLACK")
+        white_player_name.set('White Player: ' + player2.get())
+        black_player_name.set('Black Player: ' + player1.get() + "\n\n")
     else:
-        print("MCTS - WHITE, Random - BLACK")
+        white_player_name.set('White Player: ' + player1.get())
+        black_player_name.set('Black Player: ' + player2.get() + "\n\n")
 
     while game.status == AbstractBoardStatus.ONGOING:
         if turn == 0:
-            _, proposed = p.propose_action(game, None, game.legal_action_mask())
+            if player2.get() == "MCTS":
+                proposed = m.run_sims(game) if update_Qvalues.get() == "Update Q" else m.make_move(game)
+            else:
+                _, proposed = p.propose_action(game, None, game.legal_action_mask())
         if turn == 1:
-            proposed = m.run_sims(game)
+            if player1.get() == "MCTS":
+                proposed = m.run_sims(game) if update_Qvalues.get() == "Update Q" else m.make_move(game)
+            else:
+                _, proposed = p.propose_action(game, None, game.legal_action_mask())
         turn = 1 - turn
         game.push(proposed)
         l = game.populate_board()
@@ -148,9 +174,8 @@ def run_game():
         white_utility_var.set('White Utility: ' + str(game.get_white_utility()))
         black_utility_var.set('Black Utility: ' + str(game.get_black_utility()) + "\n")
         possible_actions_var.set(possible_actions_string())
-        wld_var.set(str(wld[0]) + " - " + str(wld[1]) + " - "  + str(wld[2]) + " (Wins, Losses, Draws)")
+        wld_var.set(str(wld[0]) + " - " + str(wld[1]) + " - "  + str(wld[2]) + " (Wins, Losses, Draws)\n\n")
         root.update()
-    time.sleep(2)
     if game.status == AbstractBoardStatus.BLACK_WIN:
         return 0 if init_turn == 0 else 1
     elif game.status == AbstractBoardStatus.WHITE_WIN:
@@ -169,7 +194,13 @@ def restart_game():
     global game_status_var, white_utility_var, black_utility_var
 
     game = Silverman45ChessBoard()
-    turn = 0
+    turn = random.randint(0, 1)
+    if turn == 0:
+        white_player_name.set('White Player: ' + player2.get())
+        black_player_name.set('Black Player: ' + player1.get() + "\n\n")
+    else:
+        white_player_name.set('White Player: ' + player1.get())
+        black_player_name.set('Black Player: ' + player2.get() + "\n\n")
     l = game.populate_board()
     for i in range(len(l)):
         board_tiles[i // 4][i % 4].delete('all')
@@ -189,5 +220,22 @@ update.grid(column=0, row=0)
 run.grid(column=1, row=0)
 restart.grid(column=2, row=0)
 run100.grid(column=3, row=0)
+
+options = ["Update Q", "Exploit Q"]
+drop = OptionMenu(btm_frame, update_Qvalues, *options)
+drop.grid(column=6, row=0)
+
+players = ["MCTS", "Random"]
+p1 = OptionMenu(btm_frame, player1, *players)
+p2 = OptionMenu(btm_frame, player2, *players)
+p1_text = Label(btm_frame, text='  Player 1:', font=('Modern', 12, 'bold'))
+p2_text = Label(btm_frame, text=' Player 2:', font=('Modern', 12, 'bold'))
+p1_text.grid(column=7, row=0)
+p1.grid(column=8, row=0)
+p2_text.grid(column=9, row=0)
+p2.grid(column=10, row=0)
+
+exit_button = Button(btm_frame, text="Exit", command=root.destroy)
+exit_button.grid(column=11, row=0)
 
 root.mainloop()
